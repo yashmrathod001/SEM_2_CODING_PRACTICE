646a42f1b8a68e74fc0529a9e070208f7346385b
yashmrathod001 <yashmrathod123@gmail.com>
2026-05-23 19:00:35 +0530

    escape sequences

---
+++ python/string/escape_sequences.py
@@ -0,0 +1,12 @@
+# enscape strings with escape sequences
+print("Hello\nWorld")  # Newline    
+print("Tab\tSeparated")  # Tab
+print("Backslash\\")  # Backslash
+print("Single Quote: \'")  # Single Quote
+print("Double Quote: \"")  # Double Quote
+print("Carriage Return\rOverwrite")  # Carriage Return
+print("Unicode Character: \u03A9")  # Unicode Character (Omega)
+print("Octal Value: \101")  # Octal Value (A)
+print("Hexadecimal Value: \x41")  # Hexadecimal Value (A)
+# Raw string to ignore escape sequences
+print(r"Raw String: \n \t \\")  # Raw String
