9c3a17613f5d120c57f848cf138e285b8b7a395f
yashmrathod001 <yashmrathod123@gmail.com>
2026-05-23 18:42:17 +0530

    numaric functions in python

---
+++ python/2.python_basics/numaric_functions.py
@@ -0,0 +1,45 @@
+# numaric functions 
+# abs() - returns the absolute value of a number
+print(abs(-5)) # 5
+# round() - rounds a number to the nearest integer
+print(round(3.6)) # 4
+# pow() - returns the value of a number raised to the power of another number
+print(pow(2, 3)) # 8
+# max() - returns the largest of the given numbers
+print(max(1, 5, 3)) # 5
+# min() - returns the smallest of the given numbers
+print(min(1, 5, 3)) # 1
+# sum() - returns the sum of all the items in an iterable
+numbers = [1, 2, 3, 4, 5]
+print(sum(numbers)) # 15
+# divmod() - returns a tuple containing the quotient and the remainder when dividing two numbers
+print(divmod(10, 3)) # (3, 1)
+# pow() - returns the value of a number raised to the power of another number
+print(pow(2, 3)) # 8
+
+# math module functions
+import math
+# math.sqrt() - returns the square root of a number
+print(math.sqrt(16)) # 4.0
+# math.ceil() - returns the smallest integer greater than or equal to a number
+print(math.ceil(3.2)) # 4
+# math.floor() - returns the largest integer less than or equal to a number
+print(math.floor(3.7)) # 3
+# math.factorial() - returns the factorial of a number
+print(math.factorial(5)) # 120
+# math.gcd() - returns the greatest common divisor of two numbers   
+print(math.gcd(12, 15)) # 3
+# math.sin() - returns the sine of an angle in radians
+print(math.sin(math.pi / 2)) # 1.0
+# math.cos() - returns the cosine of an angle in radians
+print(math.cos(0)) # 1.0
+# math.tan() - returns the tangent of an angle in radians
+print(math.tan(math.pi / 4)) # 1.0
+# math.log() - returns the natural logarithm of a number
+print(math.log(math.e)) # 1.0
+# math.exp() - returns e raised to the power of a number
+print(math.exp(1)) # 2.718281828459045
+
+
+
+
