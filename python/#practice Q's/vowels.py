f7d2390f8cf9e4353bd7feaf979303b33140b9ab
yashmrathod001 <yashmrathod123@gmail.com>
2026-05-27 15:29:51 +0530

    add a new file

---
+++ python/#practice Q's/vowels.py
@@ -0,0 +1,11 @@
+a = "radhe radhe "
+n = list(a)
+print(n)
+print(len(n))
+print(n[1])
+i=0
+cou = 0
+for n[i] in range(len(n)) :
+    if a[i]== "a" : #or a[i]== "e" or a[i]== "i" or a[i]== "o" or a[i]== "u" :
+        cou = cou + 1
+        print(a)
